a204c8cddde8731ee72a734357488f4f552a4997
TamNguyen372 <tamudpm@gmail.com>
2022-03-04 17:06:42 +0700

    calculator

---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Title = (props) => {
+    return ( <
+        div className = "calculator-title" > { props.title } <
+        /div>
+    )
+}
+
+export default Title;
---
+++ src/components/button.js
@@ -0,0 +1,10 @@
+const Button = (props) => {
+    return ( <
+        input className = { props.className }
+        type = "button"
+        value = { props.label }
+        onClick = { props.handleClick }
+        />
+    );
+}
+export default Button;
